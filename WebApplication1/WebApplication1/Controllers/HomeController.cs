a2e2a7b8591ced6650cf8012a0fe8bd36130ca2d
ozererol <ozer@giz.com.tr>
2017-03-19 16:44:59 +0300

    viewvBag

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -22,6 +22,7 @@ namespace WebApplication1.Controllers
                 ViewData["rest"] += ViewData["rest"].ToString() + i.ToString();
             }
 
+            ViewBag["denee"] = "deneme";
             return View();
         }
 
